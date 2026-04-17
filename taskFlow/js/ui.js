314aed0b8ff0e82042d33d865c8fb27c7eae2f0b
Harsh Dixit <dixitharsh248@gmail.com>
2026-04-17 22:39:07 +0530

    change the flow of button

--- taskFlow/js/ui.js
+++ taskFlow/js/ui.js
@@ -1,57 +1,72 @@
-const task_list=document.getElementById('task-list');
+const task_list = document.getElementById('task-list');
 
-function renderTask(){
-    task_list.innerHTML="";
+function renderTask() {
+    task_list.innerHTML = "";
 
-    const table=document.createElement('table')
-    table.className="tableClass";
+    const table = document.createElement('table')
+    table.className = "tableClass";
 
-    const tablebody=document.createElement('tbody');
+    const tablebody = document.createElement('tbody');
 
     //get data from the state.js
 
-    const tasks=getTasks();
+    const tasks = getFilteredTasks();
 
-    tasks.forEach((task)=>{
+    tasks.forEach((task) => {
 
-        const row=document.createElement('tr');
+        const row = document.createElement('tr');
 
-        const column1=document.createElement('td');
-        column1.innerText=task.text;
+        const column1 = document.createElement('td');
+        column1.innerText = task.text;
 
-        const column2=document.createElement('td');
-        column2.innerText=task.status;
+        const column2 = document.createElement('td');
+        column2.innerText = task.status;
 
-        const column3=document.createElement('td');
-        const deleteButton=document.createElement('button');
-        deleteButton.innerText="Delete";
+        const column3 = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "Delete";
 
-        const editButton=document.createElement('button');
-        editButton.innerText="Edit";
+        const editButton = document.createElement('button');
+        editButton.innerText = "Edit";
 
-        const todoButton=document.createElement('button');
-        todoButton.innerText=task.status==="done" ? "Undo" : "Done";
+        const todoButton = document.createElement('button');
+        todoButton.innerText = task.status === "done" ? "Undo" : "Done";
 
-        deleteButton.addEventListener("click",function (){
-             deleteTask(task.id);
-             renderTask();
-        });
-
-        editButton.addEventListener("click",function(){
-            const newText = prompt("Edit your task:", task.text);
-                if (newText === null || newText.trim() === "") return;
-                editTask(task.id, newText);
-                renderTask();
-        })
-        todoButton.addEventListener("click", function () {
-
-         toggleTaskStatus(task.id);
+        deleteButton.addEventListener("click", function () {
+            deleteTask(task.id);
             renderTask();
         });
 
-         column3.appendChild(editButton);
-         column3.appendChild(deleteButton);
-         column3.appendChild(todoButton);
+        editButton.addEventListener("click", function () {
+            const newText = prompt("Edit your task:", task.text);
+            if (newText === null || newText.trim() === "") return;
+            editTask(task.id, newText);
+            renderTask();
+        })
+        todoButton.addEventListener("click", function () {
+
+            toggleTaskStatus(task.id);
+            renderTask();
+        });
+
+        document.getElementById("all-btn").addEventListener("click", () => {
+            currentFilter = "all";
+            renderTask();
+        });
+
+        document.getElementById("todo-btn").addEventListener("click", () => {
+            currentFilter = "todo";
+            renderTask();
+        });
+
+        document.getElementById("done-btn").addEventListener("click", () => {
+            currentFilter = "done";
+            renderTask();
+        });
+
+        column3.appendChild(editButton);
+        column3.appendChild(deleteButton);
+        column3.appendChild(todoButton);
 
         row.appendChild(column1);
         row.appendChild(column2);
